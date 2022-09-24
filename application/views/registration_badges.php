99a9d922ae460b589ba5451d78970852ced0ea93
Shahid Iqbal <shahid@appmakers.pk>
2022-09-24 17:27:38 +0500

    badge style update

--- application/views/registration_badges.php
+++ application/views/registration_badges.php
@@ -13,30 +13,22 @@
                     <div class="col-sm-12 <?= ($key != 0 && $key % 10 == 0) ? ' page-break' : ''; ?>">
                 <?php endif; ?>
                 <div class="pull-left badges">
-                    <div class="col-sm-12 badge-container">
-                        <table class="table table-hover to-right urdu-direction registration">
-                            <tbody>
-                            <tr class="text-center">
-                                <th colspan="4">
-                                    <?= $badge_header; ?>
-                                    <br>
-                                   <small class="small"><?= $badge_sub_heading; ?></small>
-                                </th>
-                            </tr>
-                            <tr>
-                                <th colspan="2" class="text-right badge-name"><?= $badge->name; ?></th>
-                                <td colspan="2">
-                                    <span class="pull-right"><?= $badge->ideology_status; ?></span>
-                                    <span class="pull-left reg-number <?= $badge->participant_id < 10 ? 'u-10' : ($badge->participant_id < 99 ? 'u-100' : '') ?>"><?= $badge->participant_id; ?></span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan="2"><?= lang('branch') . ' : ' . $badge->city_name; ?></td>
-                                <td><small><?= $badge_footer; ?></small></td>
-                                <td class="signature"></td>
-                            </tr>
-                            </tbody>
-                        </table>
+                    <div class="col-sm-12 badge-container rtl" >
+                        <p class="text-center mb-0"><?= $badge_header ?></p>
+                        <div style="position: relative">
+                            <p class="badge-name text-center mb-0"><?= $badge->name; ?></p>
+                            <span style="position: absolute; top:0; left:0"
+                                  class="pull-left reg-number <?= $badge->participant_id < 10 ? 'u-10' : ($badge->participant_id < 99 ? 'u-100' : '') ?>"><?= $badge->participant_id; ?></span>
+                        </div>
+
+                        <div class="row text-center">
+                            <div class="col-sm-6"><?= lang('branch') . ' : ' . $badge->city_name; ?></div>
+                            <div class="col-sm-6"><?= $badge->ideology_status; ?></div>
+                        </div>
+                        <div class="row ">
+                            <div class="col-sm-3 signature"></div>
+                            <div class="col-sm-9 text-left"><?= $badge_footer; ?></div>
+                        </div>
                     </div>
                 </div>
                 <?php if (($key) % 2 != 0): ?>
@@ -56,13 +48,25 @@
     </div><!-- /.box-footer -->
 </div>
 <style>
+.rtl {
+    direction: rtl;
+}
     .badges table {
         table-layout: fixed;
+    }
+
+    .pb-0 {
+        padding-bottom: 0;
+    }
+
+    .mb-0 {
+        margin-bottom: 0;
     }
 
     .badges {
         position: relative;
         -webkit-print-color-adjust: exact;
+        padding-top: 0;
     }
 
     .badges:before {
@@ -74,7 +78,7 @@
         width: 100%;
         height: 100%;
         opacity: 0.1;
-        background-image: url('/assets/tanzeem-logo.jpeg?v=1') !important;
+        background-image: url('/assets/tanzeem-logo.jpeg') !important;
         background-repeat: no-repeat !important;;
         background-position: 50% 0 !important;;
         background-size: contain !important;
@@ -86,8 +90,8 @@
     }
 
     .badges .signature {
-        width: 100%;
-        content: url('/assets/signature.jpeg?v=2');
+        height: 100%;
+        content: url('/assets/signature.jpeg');
         background-size: contain;
         background-repeat: no-repeat;
         background-position: center;
@@ -107,7 +111,60 @@
     }
 
     .badge-name {
-        font-size: 3rem;
+        font-size: 5rem;
     }
 
+@media print {
+    .col-sm-1, .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-sm-10, .col-sm-11, .col-sm-12 {
+        float: left;
+    }
+
+    .col-sm-12 {
+        width: 100%;
+    }
+
+    .col-sm-11 {
+        width: 91.66666667%;
+    }
+
+    .col-sm-10 {
+        width: 83.33333333%;
+    }
+
+    .col-sm-9 {
+        width: 75%;
+    }
+
+    .col-sm-8 {
+        width: 66.66666667%;
+    }
+
+    .col-sm-7 {
+        width: 58.33333333%;
+    }
+
+    .col-sm-6 {
+        width: 50%;
+    }
+
+    .col-sm-5 {
+        width: 41.66666667%;
+    }
+
+    .col-sm-4 {
+        width: 33.33333333%;
+    }
+
+    .col-sm-3 {
+        width: 25%;
+    }
+
+    .col-sm-2 {
+        width: 16.66666667%;
+    }
+
+    .col-sm-1 {
+        width: 8.33333333%;
+    }
+}
 </style>
